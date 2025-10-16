6414d1b612f73779a3f2c168981a05dae9bcaad2
JhonnyMinianSD <ecuappmail@gmail.com>
2025-10-16 14:10:16 -0500

    crear rediccion para login

--- apps/ec-security-extension-data/src/app/app.routes.ts
+++ apps/ec-security-extension-data/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
+import { authGuard } from './features/auth/auth-guard';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: Login },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' },
+];
